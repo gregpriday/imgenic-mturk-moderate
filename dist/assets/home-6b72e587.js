8676bdad68a231e237f70a89028913a82d54a403
Greg Priday <greg@siteorigin.com>
2023-07-22 17:14:44 +0200

    Include dist

---
+++ dist/assets/home-6b72e587.js
@@ -0,0 +1,37 @@
+const e=`## Tutorial: How to Use our Image Rating System
+
+Hello, and welcome to our tutorial on how to use our image rating system. In this guide, we'll walk you through the steps of efficiently rating images using our user-friendly interface.
+
+### Step 1: Zooming In on Images
+
+The major feature of our rating system is the ability to get a closer look at specific parts of the image. To do this, simply move your mouse over the image. As you hover, a zoom effect will appear, allowing you to inspect different areas with ease. This feature enhances your ability to rate images accurately.
+
+### Step 2: Removing Images
+
+In some cases, an image might not meet the necessary qualifications for rating. If that's the case, you can select the "Remove" button located just below the image. This option indicates that the image doesn't qualify for rating. We'll provide more information on qualification criteria in another video, so stay tuned for that.
+
+### Step 3: Rating Images
+
+Next to the "Remove" button, you'll find the rating section. Here, you can rate the image using a scale from one to five stars. Simply choose the number of stars that best represents your assessment of the image. Once you rate it, the system will automatically move you to the next image for evaluation.
+
+### Step 4: Viewing Progress
+
+To keep track of your progress, take a look at the top of the interface. You'll see a count displaying the number of images you've rated out of the total 30 images. This count will increase as you continue rating images.
+
+### Step 5: Understanding Ratings Distribution
+
+Next to the count, you'll find a histogram. This chart shows the number of ratings for each level: from "Removed" (indicating images that don't qualify) to one to five stars (indicating the rating you gave each image).
+
+### Step 6: Accessing Instructions
+
+If you ever need a refresher on the instructions, you can easily access them by clicking the "Instructions" button located in the top right corner of the interface.
+
+### Step 7: Submitting Ratings
+
+Once you've completed rating all 30 images, it's time to submit your ratings. Click the "Submit Ratings" button at the top left corner of the interface to finalize your evaluations.
+
+### Step 8: Navigating Images
+
+To move between images during the rating process, use the left and right arrows located next to the rating section. These arrows allow you to easily navigate through the set of images.
+
+And that's it! Our simple image rating system empowers you to provide valuable feedback. Each rating you give helps us improve our services. Thank you for using our system, and happy rating!`;export{e as default};
